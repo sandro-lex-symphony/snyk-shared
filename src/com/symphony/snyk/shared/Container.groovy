92d5105de4b223a151a84dde373d593da96431d0
Sandro Lex <sandro.lex@symphony.com>
2021-04-08 16:24:55 +0200

    qqq

--- src/com/symphony/snyk/shared/Container.groovy
+++ src/com/symphony/snyk/shared/Container.groovy
@@ -5,14 +5,8 @@ package com.symphony.snyk.shared
 // 3. snyk container monitor --policy-path abc
 class Container {
     def steps
-    def nodeVersion = '14.16.1'
-
     Container(steps) {
         this.steps = steps
-        // instal nodejs LTS
-        //this.steps.sh 'sh wget https://nodejs.org/dist/v14.16.1/node-v14.16.1-linux-x64.tar.xz && tar -xf node-v14.16.1-linux-x64.tar.xz --directory /usr/local --strip-components 1'
-        // install snyk
-        //this.steps.sh 'npm install -g snyk'
     }
 
     def hello(param) {
@@ -20,7 +14,7 @@ class Container {
         steps.sh 'pwd'
     }
 
-    def test(image) {
+    def scan(image) {
         steps.withCredentials([string(credentialsId: 'SNYK_API_TOKEN', variable: 'SNYK_TOKEN')]) {
             steps.sh 'snyk auth '+SNYK_TOKEN  
         }
