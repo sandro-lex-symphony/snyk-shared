f805bd964f253ad9d75f3a38f7d66df9c9d956e5
Sandro Lex <sandro.lex@symphony.com>
2021-04-08 15:15:34 +0200

    typo

--- src/com/symphony/snyk/shared/ContainerScan.groovy
+++ src/com/symphony/snyk/shared/ContainerScan.groovy
@@ -3,7 +3,7 @@ package com.symphony.snyk.shared
 // 1. snyk auth token
 // 2. snyk container test --severity x --policy-path y image:tag
 // 3. snyk container monitor --policy-path abc
-def containerScan(param) {
+def ContainerScan(param) {
     echo 'scan this ' + param
     sh 'pwd'
 }
