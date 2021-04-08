956787cb66b07fd2c38ba942f0004dfa0595b0da
Sandro Lex <sandro.lex@symphony.com>
2021-04-08 16:42:34 +0200

    reverted to Container

--- src/com/symphony/snyk/shared/Container.groovy
+++ src/com/symphony/snyk/shared/Container.groovy
@@ -3,10 +3,10 @@ package com.symphony.snyk.shared
 // 1. snyk auth token
 // 2. snyk container test --severity x --policy-path y image:tag
 // 3. snyk container monitor --policy-path abc
-class ContainerScan {
+class Container {
     def steps
     def nodejs_version = '14.16.1'
-    ContainerScan(steps) {
+    Container(steps) {
         this.steps = steps
         // install nodejs
         steps.sh "wget https://nodejs.org/dist/v${nodejs_version}/node-v${nodejs_version}-linux-x64.tar.xz && tar -xf node-v}$nodejs_version}-linux-x64.tar.xz --directory /usr/local --strip-components 1'
