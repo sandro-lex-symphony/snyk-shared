7a2b648ba1981c5de7004948a439c93f4847796d
Sandro Lex <sandro.lex@symphony.com>
2021-04-08 16:29:13 +0200

    reverted to containerscan

--- src/com/symphony/snyk/shared/ContainerScan.groovy
+++ src/com/symphony/snyk/shared/ContainerScan.groovy
@@ -3,9 +3,9 @@ package com.symphony.snyk.shared
 // 1. snyk auth token
 // 2. snyk container test --severity x --policy-path y image:tag
 // 3. snyk container monitor --policy-path abc
-class Container {
+class ContainerScan {
     def steps
-    Container(steps) {
+    ContainerScan(steps) {
         this.steps = steps
     }
 
