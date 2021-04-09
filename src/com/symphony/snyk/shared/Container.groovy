9fe5f3de92afc8b92c84e92502c70d2d2646846d
Sandro Lex <sandro.lex@symphony.com>
2021-04-09 14:40:01 +0200

    clean up

--- src/com/symphony/snyk/shared/Container.groovy
+++ src/com/symphony/snyk/shared/Container.groovy
@@ -3,6 +3,8 @@ package com.symphony.snyk.shared
 // 1. snyk auth token
 // 2. snyk container test --severity x --policy-path y image:tag
 // 3. snyk container monitor --policy-path abc
+// TODO: add --file=Dockerfile
+// TODO: add --exclude base image
 class Container {
     def steps
     def token
