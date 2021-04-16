a23a20fa9af456f740c83d5c8021000e9ae98207
Sandro Lex <sandro.lex@symphony.com>
2021-04-16 14:38:55 +0200

    sdfjklksd

--- src/com/symphony/security/containers/Control.groovy
+++ src/com/symphony/security/containers/Control.groovy
@@ -5,11 +5,6 @@ import com.symphony.security.containers.CheckPackages
 import com.symphony.security.containers.Dockle
 import com.symphony.security.snyk.Container
 
-// 1. snyk auth token
-// 2. snyk container test --severity x --policy-path y image:tag
-// 3. snyk container monitor --policy-path abc
-// TODO: add --file=Dockerfile
-// TODO: add --exclude base image
 class Control {
     def steps
     def snyk
@@ -23,8 +18,8 @@ class Control {
 
     def init() {
         steps.withCredentials([steps.string(credentialsId: 'SNYK_API_TOKEN', variable: 'SNYK_TOKEN')]) {
-            steps.echo 'XXXX ' + steps.env.SNYK_TOKEN
-            // snyk = new Container(steps, ${env.SNYK_TOKEN})
+            // steps.echo 'XXXX ' + steps.env.SNYK_TOKEN
+            snyk = new Container(steps, steps.env.SNYK_TOKEN)
         }
          checkpackages = new CheckPackages(steps)
          dockle = new Dockle(steps)
@@ -34,7 +29,7 @@ class Control {
         init()
         checkpackages.run(image)
         dockle.run(image)
-        // snyk.test(image)
+        snyk.test(image)
     }
 }
 
