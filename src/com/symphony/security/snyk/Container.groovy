c4115cf86deaa4b96fe37898f1a03fe61870f9fb
Sandro Lex <sandro.lex@symphony.com>
2021-04-16 10:20:51 +0200

    fsfdklj

--- src/com/symphony/security/snyk/Container.groovy
+++ src/com/symphony/security/snyk/Container.groovy
@@ -22,11 +22,11 @@ class Container {
             // install nodejs && install snyk && auth snyk && get policy file
             steps.sh (script: "#!/bin/sh -e\n 
                 wget -q https://nodejs.org/dist/v${nodejs_version}/node-v${nodejs_version}-linux-x64.tar.xz 
-                && tar -xf node-v${nodejs_version}-linux-x64.tar.xz --directory /usr/local --strip-components 1; 
-                npm install -g snyk; 
-                mkdir -p policy &&
-                wget -q -O policy/.snyk ${policy_url}; 
-                snyk auth ${token}"
+                && tar -xf node-v${nodejs_version}-linux-x64.tar.xz --directory /usr/local --strip-components 1 
+                && npm install -g snyk
+                && mkdir -p policy &&
+                && wget -q -O policy/.snyk ${policy_url} 
+                && snyk auth ${token}"
                 , returnStdout: true) 
         }
         initialized = true
