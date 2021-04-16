7083b598d62585f35bbd07b0fb2fe24dc6f82cba
Sandro Lex <sandro.lex@symphony.com>
2021-04-16 14:35:44 +0200

    dfsljdk

--- src/com/symphony/security/containers/Control.groovy
+++ src/com/symphony/security/containers/Control.groovy
@@ -23,7 +23,7 @@ class Control {
 
     def init() {
         steps.withCredentials([steps.string(credentialsId: 'SNYK_API_TOKEN', variable: 'SNYK_TOKEN')]) {
-            steps.echo 'XXXX ' + SNYK_TOKEN
+            steps.echo 'XXXX ' + steps.env.SNYK_TOKEN
             // snyk = new Container(steps, ${env.SNYK_TOKEN})
         }
          checkpackages = new CheckPackages(steps)
