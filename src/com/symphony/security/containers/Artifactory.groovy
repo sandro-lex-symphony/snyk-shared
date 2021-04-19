98153934c264e67a2b521669be0944aec2b02d8a
Sandro Lex <sandro.lex@symphony.com>
2021-04-19 13:05:39 +0200

    ffdlgkj

---
+++ src/com/symphony/security/containers/Artifactory.groovy
@@ -0,0 +1,30 @@
+package com.symphony.security.containers
+
+// 1. login with creds
+// 2. tag && push image 
+class Artifactory {
+    
+    private def steps
+    private Boolean initialized = false
+    private String artifactory_url = "artifact.symphony.com"
+   
+
+    Artifactory(steps) {
+        this.steps = steps
+    }
+
+    def init() {
+        if (!initialized) {
+            steps.withCredentials([steps.usernamePassword(credentialsId: 'container_registry_svc_user', usernameVariable: 'username', passwordVariable: 'password')]) {
+            steps.sh (script: "#!/bin/sh -e\n docker login --username ${username} --password ${password} ${artifactory_url})", returnStdout: true)
+        }
+        initialized = true
+    }
+
+    def push(image_local, image_repository) {
+        init()
+        steps.sh (script: "#!/bin/sh -e\n docker tag ${image_local} ${artifactory_url}/${image_repository}", returnStdout: true)
+        steps.sh (script: "#!/bin/sh -e\n docker push ${artifactory_url}/${image_repository}", returnStdout: true)
+    }
+}
+
