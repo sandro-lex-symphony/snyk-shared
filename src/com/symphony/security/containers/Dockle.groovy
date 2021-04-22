0461d143bf83a1879bb3151c2cb1bc36bf9ace91
Sandro Lex <sandro.lex@symphony.com>
2021-04-22 16:18:19 +0200

    add .dockleignre

--- src/com/symphony/security/containers/Dockle.groovy
+++ src/com/symphony/security/containers/Dockle.groovy
@@ -8,10 +8,11 @@ class Dockle {
     
     private def steps
     private Boolean initialized = false
-    private String policy_url = 'https://raw.githubusercontent.com/sandro-lex-symphony/docker-images/master/packages/blacklist.txt'
-    private String policy_file = 'policy/dockleignore'
+    private String base_image_policy_url = 'https://raw.githubusercontent.com/sandro-lex-symphony/docker-images/master/debian-policy/dockle-base_image'
+    private String production_image_policy_url = 'https://raw.githubusercontent.com/sandro-lex-symphony/docker-images/master/debian-policy/dockle-production_image'
+    private String policy_file = '.dockleignore'
     private String dockle_bin_url = "https://github.com/sandro-lex-symphony/checkpackages/releases/download/v0.1/dockle"
-    private String conf_base_image = "-i DKL-DI-0003 -i CIS-DI-0001  -i CIS-DI-0006"
+    private String conf_base_image = "-i DKL-DI-0003 -i CIS-DI-0001  -i CIS-DI-0006 -i CIS-DI-0005"
 
     Dockle(steps) {
         this.steps = steps
@@ -19,7 +20,7 @@ class Dockle {
 
     def init() {
         if (!initialized) {
-            //steps.sh (script: "#!/bin/sh -e\n mkdir -p policy && wget -q -O ${policy_file} ${policy_url}", returnStdout: true)
+            steps.sh (script: "#!/bin/sh -e\n mkdir -p policy && wget -q -O policy/dockle_base_image ${base_image_policy_url} && wget -q -O policy/dockle_production_image ${production_image_policy_url}", returnStdout: true)
             steps.sh (script: "#!/bin/sh -e\n wget -q -O dockle ${dockle_bin_url}; chmod +x dockle", returnStdout: true)    
         }
         initialized = true
@@ -27,11 +28,13 @@ class Dockle {
 
     def run(image, ignores) {
         init()
-        def out = steps.sh (script: "#!/bin/sh -e\n ./dockle ${conf_base_image} --exit-code 0 ${image} | egrep -v IGNORE", returnStdout: true)
+        def out = steps.sh (script: "#!/bin/sh -e\n cp policy/dockle_production_image .dockleignore && ./dockle --exit-code 0 ${image} | egrep -v IGNORE", returnStdout: true)
         steps.echo out
     }  
     
     def base_image(image) {
-       run(image, conf_base_image) 
+        init()
+        def out = steps.sh (script: "#!/bin/sh -e\n cp policy/dockle_base_image .dockleignore && ./dockle --exit-code 0 ${image} | egrep -v IGNORE", returnStdout: true)
+        steps.echo out
     }
 }
