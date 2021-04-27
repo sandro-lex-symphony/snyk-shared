a85a55ea21e60d8dedfaa98303917998cdab5bd6
Sandro Lex <sandro.lex@symphony.com>
2021-04-27 15:59:03 +0200

    debug

--- src/com/symphony/security/containers/Builder.groovy
+++ src/com/symphony/security/containers/Builder.groovy
@@ -17,7 +17,7 @@ class Builder {
     def buildAndPublish(image_name, dockerfile, context_path) {
         // docker build
         steps.echo "### Building container image ${image_name}"
-        steps.sh (script: "DOCKER_BUILDKIT=1 DOCKER_CONTENT_TRUST=1 docker build --no-cache -f ${dockerfile} -t ${image_name} ${context_path}", returnStdout: true)
+        steps.sh (script: "DOCKER_BUILDKIT=1 DOCKER_CONTENT_TRUST=1 docker build --progress plain --no-cache -f ${dockerfile} -t ${image_name} ${context_path}", returnStdout: true)
 
         // security checks
         def security = new Control(this)
