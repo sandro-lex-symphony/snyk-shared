e20d9c4af8c5d51b5da94674e2959c06b86b4fbc
Sandro Lex <sandro.lex@symphony.com>
2021-04-27 15:05:18 +0200

    add builder file

---
+++ src/com/symphony/security/containers/Builder.groovy
@@ -0,0 +1,29 @@
+package com.symphony.security.containers
+
+ @Library('SnykShared@master')                                                                                                                                                                    
+import com.symphony.security.containers.Control
+import com.symphony.security.containers.Artifactory
+
+
+
+class Builder {
+    def steps
+    def artifactory_repo = 'slex-reg-test/'
+    
+    Builder(steps) {
+        this.steps = steps
+    }
+
+    def build(image_name, dockerfile, context_path) {
+        // docker build
+        steps.echo "### Building container image ${image}"
+        steps.sh (script: "DOCKER_BUILDKIT=1 DOCKER_CONTENT_TRUST=1 docker build --no-cache -f ${dockerfile} -t ${image_name} ${context_path}", returnStdout: true)
+
+        // security checks
+        def security = new Control(this)
+        security.base_image(image_name)
+
+        // push to repo
+        def artifactory = new Artifactory(this)
+        artifactory.push(image_name, artifactory_repo + image_name)
+    }
