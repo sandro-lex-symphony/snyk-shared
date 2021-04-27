898e0e8c43f8b5a84506a4bd522fa852a6dd8bbc
Sandro Lex <sandro.lex@symphony.com>
2021-04-27 16:14:04 +0200

    param buildkit

--- src/com/symphony/security/containers/Builder.groovy
+++ src/com/symphony/security/containers/Builder.groovy
@@ -9,15 +9,33 @@ import com.symphony.security.containers.Artifactory
 class Builder {
     def steps
     def artifactory_repo = 'slex-reg-test/'
+    def buildkit = "DOCKER_BUILDKIT=1"
+    def content_trust = "DOCKER_CONTENT_TRUST=1"
     
     Builder(steps) {
         this.steps = steps
     }
 
+    def setBuildkit(v) {
+        if (v) {
+            buildkit = "DOCKER_BUILDKIT=1"
+        } else {
+            buildkit = "DOCKER_BUILDKIT=0"
+        }
+    }
+
+    def setContentTrust(v) {
+        if (v) {
+            content_trust = "DOCKER_CONTENT_TRUST=1"
+        } else {
+            content_trust = "DOCKER_CONTENT_TRUST=0"
+        }
+    }
+
     def buildAndPublish(image_name, dockerfile, context_path) {
         // docker build
         steps.echo "### Building container image ${image_name}"
-        steps.sh (script: "DOCKER_BUILDKIT=1 DOCKER_CONTENT_TRUST=1 docker build --progress plain --no-cache -f ${dockerfile} -t ${image_name} ${context_path}", returnStdout: true)
+        steps.sh (script: "${buildkit} ${content_trust} docker build --progress plain --no-cache -f ${dockerfile} -t ${image_name} ${context_path}", returnStdout: true)
 
         // security checks
         def security = new Control(this)
