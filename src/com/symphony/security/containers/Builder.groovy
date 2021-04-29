e7f62efea6da0de7e85d451dceb1e25116bd3083
Sandro Lex <sandro.lex@symphony.com>
2021-04-29 15:44:12 +0200

    qwew

--- src/com/symphony/security/containers/Builder.groovy
+++ src/com/symphony/security/containers/Builder.groovy
@@ -12,6 +12,8 @@ class Builder {
     def buildkit = "DOCKER_BUILDKIT=1"
     def content_trust = "DOCKER_CONTENT_TRUST=1"
     def flags = ''
+    def cache_args = '--no-cache'
+    def pull_args = '--pull'
     
     Builder(steps) {
         this.steps = steps
@@ -33,31 +35,46 @@ class Builder {
         }
     }
 
+    def noCacheArg(v) {
+        if (v == true) {
+            cache_args = '--no-cache'
+        } else {
+            cache_args = ''
+        }
+    }
+
+    def pullArg(v) {
+        if (v == true) {
+            pull_args = '--pull'
+        } else {
+            pull_args = ''
+        }
+    }
+
     def flags(v) {
         flags = v
     }
 
     def dockerBuild(image_name, dockerfile, context_path) {
         steps.echo "### Building container image ${image_name}"
-        steps.sh (script: "${this.buildkit} ${this.content_trust} docker build --no-cache --pull ${flags} -f ${dockerfile} -t ${image_name} ${context_path}", returnStdout: true)
-        steps.echo "### finished building"
+        steps.sh (script: "${this.buildkit} ${this.content_trust} docker build ${cache_args} ${pull_args} ${flags} -f ${dockerfile} -t ${image_name} ${context_path}", returnStdout: true)
+        steps.echo "### Done building ${image_name}"
     }
 
     def buildAndPublish(image_name, dockerfile, context_path) {
         // docker build
-        steps.echo "### Building container image ${image_name}"
-        steps.sh (script: "${this.buildkit} ${this.content_trust} docker build ${flags} --no-cache -f ${dockerfile} -t ${image_name} ${context_path}", returnStdout: true)
-        steps.echo "### finished building"
+        dockerBuild(image_name, dockerfile, context_path)
 
         // security checks
-        steps.echo "### SECURITY CHECKS"
+        steps.echo "### Running Security Checks for ${image_name}"
         def security = new Control(this.steps)
         security.base_image(image_name, dockerfile)
-        steps.echo "### END SECURITY"
+        steps.echo "### Done security checks for ${image_name}"
 
         // push to repo
-        steps.echo "### GOING ARTIFACTORY"
+        steps.echo "### Going to push ${image_name} to Artifactory"
         def artifactory = new Artifactory(this.steps)
         artifactory.push(image_name, artifactory_repo + image_name)
+        steps.echo "### Done pushing ${image_name} to Artifactory"
     }
 }
